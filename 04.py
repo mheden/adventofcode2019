70c5f1035dcaab2080a9aebc809a58fb165835b1
Mikael Heden <mikael@heden.net>
2019-12-04 08:43:56 +0100

    Day 04

---
+++ 04.py
@@ -0,0 +1,54 @@
+from utils import read_file, manhattan_distance
+
+
+def check_password(pwd):
+    doubles = 0
+    fail = 0
+    last_digit = None
+    for p in pwd:
+        if p == last_digit:
+            doubles += 1
+        if last_digit is not None and p < last_digit:
+            fail += 1
+        last_digit = p
+    return doubles > 0 and fail == 0
+
+
+def check_password2(pwd):
+    doubles = [0 for _ in range(0, 10)]
+    fail = 0
+    last_digit = None
+    for p in pwd:
+        if p == last_digit:
+            doubles[int(p)] += 1
+        if last_digit is not None and p < last_digit:
+            fail += 1
+        last_digit = p
+    try:
+        return doubles.index(1) and fail == 0
+    except ValueError:
+        return False
+
+
+print("#--- part1 ---#")
+
+assert(check_password('111111') is True)
+assert(check_password('223450') is False)
+assert(check_password('123789') is False)
+
+success = 0
+for pwd in range(168630, 718098):
+    success += check_password(str(pwd))
+print(success)
+
+
+print("#--- part2 ---#")
+
+assert(check_password2('112233') is True)
+assert(check_password2('123444') is False)
+assert(check_password2('111122') is True)
+
+success = 0
+for pwd in range(168630, 718098):
+    success += check_password2(str(pwd))
+print(success)
