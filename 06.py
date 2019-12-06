e84dcf9ead78945f2219b72cabce83d922b96407
Mikael Heden <mikael@heden.net>
2019-12-06 08:06:22 +0100

    Day 06

---
+++ 06.py
@@ -0,0 +1,73 @@
+from utils import read_file
+
+
+def calc_parents(connections, node):
+    if node == 'COM':
+        return 0
+    else:
+        return 1 + calc_parents(connections, connections[node])
+
+
+def get_connections(map_):
+    connections = {}
+    for line in map_:
+        from_, to_ = line.split(')')
+        connections[to_] = from_
+    return connections
+
+
+def orbits(map_):
+    connections = get_connections(map_)
+    return sum([calc_parents(connections, n) for n in connections.keys()])
+
+
+def get_ancestors(connections, node):
+    if node == 'COM':
+        return [node]
+    else:
+        return [node] + get_ancestors(connections, connections[node])
+
+
+def transfers(map_, from_, to_):
+    connections = get_connections(map_)
+    a = get_ancestors(connections, from_)
+    b = get_ancestors(connections, to_)
+    common = [i for i in a if i in b]
+    return (a.index(common[0]) - 1) + (b.index(common[0]) - 1)
+
+
+print("#--- part1 ---#")
+
+data = """COM)B
+B)C
+C)D
+D)E
+E)F
+B)G
+G)H
+D)I
+E)J
+J)K
+K)L""".splitlines()
+
+assert(orbits(data) == 42)
+print(orbits(read_file('06.txt')))
+
+print("#--- part2 ---#")
+
+data = """COM)B
+B)C
+C)D
+D)E
+E)F
+B)G
+G)H
+D)I
+E)J
+J)K
+K)L
+K)YOU
+I)SAN""".splitlines()
+
+assert(transfers(data, 'YOU', 'SAN') == 4)
+print(transfers(read_file('06.txt'), 'YOU', 'SAN'))
